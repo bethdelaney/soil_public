abe1df4629bd6e7e3ab406bb762f019e945bf0ff
Matt Payne <matthewjpayne1@gmail.com>
2024-11-19 09:41:46 +0000

    added additional indices calc, and programmatic saving of thumbnails

--- scripts/get_sentinel2_timeseries.py
+++ scripts/get_sentinel2_timeseries.py
@@ -6,8 +6,9 @@ Script to access Sentinel-2 spectral reflectance values by querying GEE servers.
 
 from datetime import datetime
 import logging
-from typing import Tuple, Optional
+import os
 import sys
+from typing import Optional
 
 import ee
 import folium
@@ -20,7 +21,7 @@ import numpy as np
 from PIL import Image
 
 
-def main(project_name: str, aoi_path: str, start_date: str, end_date: str, out_png_path: Optional[str]=None) -> None:
+def main(project_name: str, aoi_path: str, start_date: str, end_date: str, out_directory: Optional[str]=None) -> None:
     """
     
     Parameters
@@ -33,8 +34,8 @@ def main(project_name: str, aoi_path: str, start_date: str, end_date: str, out_p
         the start date, in the format "YYYY-MM-DD"
     end_date : str
         the end date, in the format "YYYY-MM-DD"
-    out_png_path : str, optional, by default None
-        if supplied, the path to save an image as png for visualisation
+    out_directory : str, optional, by default None
+        if supplied, the path of the directory to save images as png for visualisation
     
     Returns
     -------
@@ -57,8 +58,8 @@ def main(project_name: str, aoi_path: str, start_date: str, end_date: str, out_p
     s2 = query_sentinel2_archive(aoi=polygon_ee, start_date=start_date, end_date=end_date)
 
     # if this argument is passed, then save an NDVI image as png
-    if out_png_path:
-        save_ndvi_thumbnail(s2.first(), out_png_path)
+    if out_directory:
+        save_index_thumbnails(s2.first(), out_directory)
 
     return
 
@@ -128,6 +129,8 @@ def convert_to_ee_geometry(gdf: gpd.GeoDataFrame) -> ee.Geometry.Polygon:
         GEE compliant geometry
     """
 
+    logger = logging.getLogger(__name__)
+
     polygon_geometry = gdf.geometry[0]
     polygon_geojson = polygon_geometry.__geo_interface__
 
@@ -136,6 +139,8 @@ def convert_to_ee_geometry(gdf: gpd.GeoDataFrame) -> ee.Geometry.Polygon:
     polygon_2d_coords = [
         [(x, y) for x, y, _ in ring] for ring in polygon_geojson['coordinates']
     ]
+
+    logger.info(f"polygon_2d_coords : {polygon_2d_coords}")
 
     return ee.Geometry.Polygon(polygon_2d_coords)
 
@@ -167,11 +172,11 @@ def query_sentinel2_archive(aoi: ee.Geometry.Polygon, start_date: str, end_date:
 
     s2 = (
         ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
-        .filterDate(start_date, end_date) # for some reason does not like how I am passing the dates
+        .filterDate(start_date, end_date)
         .filterBounds(aoi)
         .sort("system:time_start")
         # .map(lambda image: image.clip(aoi))
-        .select(["B4", "B8", "B11", "B12"])
+        .select(["B3", "B4", "B8", "B11", "B12"])
         .map(compute_indices)
         )
     
@@ -208,52 +213,73 @@ def convert_dn_to_reflectance(image: ee.Image) -> float:
 
     return reflectance
 
-def save_ndvi_thumbnail(image: ee.Image, out_path: str) -> None:
+def save_index_thumbnails(image: ee.Image, out_path_prefix: str) -> None:
     """
 
-    Saves an image as a PNG, useful for debugging.
+    Saves thumbnails for multiple indices as PNGs:
+        - Normalised Difference Vegetation Index (NDVI)
+        - Normalised Burn Ratio (NBR)
+        - Normalised Difference Water Index (NDWI)
+        - Soil Adjusted Vegetation Index (SAVI)
+
 
     Parameters
     ----------
     image : ee.Image
-        an earth engine image.
+        an earth engine image, containing the indices.
     out_path : str
-        the path to write the png to.
+        the path of the directory to write to.
     """
 
     logger = logging.getLogger(__name__)
 
-    image = image.select("NDVI")
-
-    vis_params = {
-        "max": 1,
-        "min": 0,
-        "palette": ["white", "green"]
+    vis_params_dict = {
+        "NDVI": {
+            "max": 1,
+            "min": 0,
+            "palette": ["white", "green"]
+        },
+        "NDWI": {
+            "max": 1,
+            "min": -1,
+            "palette": ["white", "blue"]
+        },
+        "NBR": {
+            "max": 1,
+            "min": -1,
+            "palette": ["white", "red"]
+        },
+        "SAVI": {
+            "max": 1,
+            "min": -1,
+            "palette": ["white", "orange"]
+        }
     }
 
-    # save the thumbnail
-    geemap.get_image_thumbnail(image,
-                               out_img=out_path,
-                               vis_params=vis_params,
-                               format="png")
+    for index_name, vis_params in vis_params_dict.items():
+        # get index image
+        index_image = image.select(index_name)
+        # construct out_path
+        out_path = os.path.join(out_path_prefix, f"{index_name}.png")
 
-    # # get the url of the image from the server
-    # url = image.getThumbUrl(vis_params)
-    # array = np.array(Image.open(url))
-    # plt.imshow(array)
-    # plt.title("NDVI")
-    # plt.show()
-    # plt.imsave(out_path, array, dpi=900)
+        # save the thumbnail
+        geemap.get_image_thumbnail(index_image,
+                                out_img=out_path,
+                                vis_params=vis_params,
+                                format="png")
+        
+        logger.info(f"saved {index_name} to {out_path} ")
+
 
     return
 
 def compute_indices(image: ee.Image) -> ee.Image:
     """
     Computes the following indices on `image` and adds the indices back to `image`:
-      - Normalised Difference Vegetation Index (NDVI)
-      - Normalised Burn Ratio (NBR)
-      - Normalised Difference Water Index (NDWI)
-      - Soil Adjusted Vegetation Index (SAVI)
+        - Normalised Difference Vegetation Index (NDVI)
+        - Normalised Burn Ratio (NBR)
+        - Normalised Difference Water Index (NDWI)
+        - Soil Adjusted Vegetation Index (SAVI)
 
     Parameters
     ----------
@@ -288,4 +314,4 @@ if __name__ == "__main__":
          aoi_path=sys.argv[3],
          start_date=sys.argv[4],
          end_date=sys.argv[5],
-         out_png_path=sys.argv[6])
+         out_directory=sys.argv[6])
