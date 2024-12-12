7b9ca146c93b38990e2b536569587eeb7b50c0af
Matthew Payne <matthewjpayne1@gmail.com>
2024-12-12 17:52:45 +0000

    parallelism, may not be working

--- scripts/get_s2/get_sentinel2_timeseries.py
+++ scripts/get_s2/get_sentinel2_timeseries.py
@@ -53,11 +53,45 @@ def main(project_name: str, aoi_path: str, start_date: str, end_date: str, out_d
     initialise(project_name=project_name)
 
     # read in shapefile
-    gdf=gpd.read_file(aoi_path)
+    gdf = gpd.read_file(aoi_path)
+    batch_size = 1 # the number of simultaneous requests to send to GEE servers
+    delay = 5 # delay between batches in seconds
 
-    # iterate over each polygon in the shapefile
-    for index, row in gdf.iterrows():
-        logger.info("broken for now")
+    # create thread locks to prevent filesystem corruption
+    csv_lock = threading.Lock()
+    logging_lock = threading.Lock()
+
+    # it's parallel time, baby!
+    # iterate over each polygon in the shapefile with list comprehension
+    with ThreadPoolExecutor() as executor:
+        # effectively divide the number of rows in `gdf` into `batch_size`
+        for i in range(0, len(gdf), batch_size):
+            # get a batch
+            batch = gdf[i : i + batch_size]
+            # for all rows in the batched gdf, run `process_polygon` for each row
+            futures = [
+                executor.submit(
+                    process_polygon,
+                    row=row,
+                    index=index,
+                    start_date=start_date,
+                    end_date=end_date,
+                    out_directory=out_directory,
+                    csv_lock=csv_lock,
+                    logging_lock=logging_lock
+                    ) 
+                    for index, row in batch.iterrows()
+                    ]
+            # wait until the current batch to finish before moving on
+            for future in as_completed(futures):
+                try:
+                    future.result()
+                except Exception as e:
+                    with logging_lock:
+                        logger.error(f"Error in thread : {e}")
+                
+            # wait to avoid setting off GEE rate limiting
+            time.sleep(delay)
 
     return
 
@@ -94,7 +128,8 @@ def process_polygon(row: gpd.GeoSeries, index:int, start_date: str, end_date: st
         # query the S2 Archive with a thread lock
         with logging_lock:
             s2 = query_sentinel2_archive(aoi=polygon_ee, start_date=start_date, end_date=end_date)
-
+            logger.info(type(s2))
+            
         # get spectral data and write to csv
         if out_directory and s2 is not None:
             # save_image_thumbnails(s2.first(), out_directory)
@@ -109,7 +144,11 @@ def process_polygon(row: gpd.GeoSeries, index:int, start_date: str, end_date: st
             # get mean index values over time, from the AOI centroid and write to CSV, with a thread lock
             with csv_lock:
                 extract_index_timeseries(s2, polygon_ee, sub_directory)
-        
+    
+    except ee.EEException as ee_e:
+        with logging_lock:
+            logger.error(f"EEException in polygon {index}: {ee_e} \n {traceback.format_exc()}")
+
     except Exception as e: # should make this more specific when I know the likely exceptions raised
         # configure traceback
         error_message = traceback.format_exc()
